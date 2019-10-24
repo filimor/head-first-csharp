00ed8a29e0ede06049cb4cda569bfea640c57add
Filipe Moreira <filimor@posteo.com.br>
2019-10-24 19:24:31 -0300

    Implementando Interfaces (nao usadas)

---
+++ BeehiveManager/INectarCollector.cs
@@ -0,0 +1,11 @@
+﻿namespace BeehiveManager
+{
+    public interface INectarCollector
+    {
+        double Nectar { get; }
+
+        void FindFlowers();
+        void GatherNectar();
+        void ReturnToHive();
+    }
+}
---
+++ BeehiveManager/IStingPatrol.cs
@@ -0,0 +1,12 @@
+﻿namespace BeehiveManager
+{
+    public interface IStingPatrol
+    {
+        int AlertLevel { get; }
+        int StingerLenght { get; set; }
+
+        bool LookForEnemies();
+        int SharpenStinger(int lenght);
+        void Sting();
+    }
+}
