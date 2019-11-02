f2c120a0bee29e9c3662ceb685611072a5048cfd
Filipe Moreira <filimor@posteo.com.br>
2019-11-02 11:05:56 -0300

    Comecando a implementar a classe Deck

---
+++ DeckOfCards/CardsComparer_bySuit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+    public class CardsComparer_bySuit : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            return x.Suit > y.Suit ?
+                1 : x.Suit < y.Suit ?
+                -1 : x.Value > y.Value ?
+                1 : x.Value < y.Value ?
+                -1 : 0;
+        }
+    }
+}
--- DeckOfCards/Deck.cs
+++ DeckOfCards/Deck.cs
@@ -8,5 +8,53 @@ namespace DeckOfCards
 {
     public class Deck
     {
+        private List<Card> _cards;
+        private Random _random = new Random();
+
+        public int Count => _cards.Count;
+
+        public Deck()
+        {
+            _cards = new List<Card>();
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 1; value <= 13; value++)
+                {
+                    _cards.Add(new Card((Card.Suits)suit,(Card.Values)value));
+                }
+            }
+        }
+
+        public Deck(Card[] initialCards)
+        {
+            _cards = new List<Card>(initialCards);
+        }
+
+        public void Add(Card cardToAdd)
+        {
+            _cards.Add(cardToAdd);
+        }
+
+        public Card Deal(int index)
+        {
+            Card cardToDeal = _cards[index];
+            _cards.RemoveAt(index);
+            return cardToDeal;
+        }
+
+        public void Shuffle()
+        {
+
+        }
+
+        public string[] GetCardNames()
+        {
+
+        }
+
+        public void Sort()
+        {
+            _cards.Sort(new CardComparer_bySuit());
+        }
     }
 }
