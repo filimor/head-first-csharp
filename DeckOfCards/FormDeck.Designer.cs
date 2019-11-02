bbbc638584fd763d75932ddd1a9c4b9f39b36b5a
Filipe Moreira <filimor@posteo.com.br>
2019-11-02 10:36:19 -0300

    Eventos do formulario

--- DeckOfCards/FormDeck.Designer.cs
+++ DeckOfCards/FormDeck.Designer.cs
@@ -28,20 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDeck));
+            this.lblDeck1 = new System.Windows.Forms.Label();
+            this.lblDeck2 = new System.Windows.Forms.Label();
+            this.lstDeck1 = new System.Windows.Forms.ListBox();
+            this.lstDeck2 = new System.Windows.Forms.ListBox();
+            this.btnSortDeck1 = new System.Windows.Forms.Button();
+            this.btnShuffleDeck2 = new System.Windows.Forms.Button();
+            this.btnShuffleDeck1 = new System.Windows.Forms.Button();
+            this.btnMoveToDeck2 = new System.Windows.Forms.Button();
+            this.btnMoveToDeck1 = new System.Windows.Forms.Button();
+            this.btnSortDeck2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblDeck1
+            // 
+            this.lblDeck1.AutoSize = true;
+            this.lblDeck1.Location = new System.Drawing.Point(12, 9);
+            this.lblDeck1.Name = "lblDeck1";
+            this.lblDeck1.Size = new System.Drawing.Size(105, 13);
+            this.lblDeck1.TabIndex = 0;
+            this.lblDeck1.Text = "Baralho 1 (10 cartas)";
+            // 
+            // lblDeck2
+            // 
+            this.lblDeck2.AutoSize = true;
+            this.lblDeck2.Location = new System.Drawing.Point(216, 9);
+            this.lblDeck2.Name = "lblDeck2";
+            this.lblDeck2.Size = new System.Drawing.Size(105, 13);
+            this.lblDeck2.TabIndex = 1;
+            this.lblDeck2.Text = "Baralho 2 (52 cartas)";
+            // 
+            // lstDeck1
+            // 
+            this.lstDeck1.FormattingEnabled = true;
+            this.lstDeck1.Location = new System.Drawing.Point(15, 25);
+            this.lstDeck1.Name = "lstDeck1";
+            this.lstDeck1.Size = new System.Drawing.Size(144, 212);
+            this.lstDeck1.TabIndex = 2;
+            // 
+            // lstDeck2
+            // 
+            this.lstDeck2.FormattingEnabled = true;
+            this.lstDeck2.Location = new System.Drawing.Point(219, 25);
+            this.lstDeck2.Name = "lstDeck2";
+            this.lstDeck2.Size = new System.Drawing.Size(144, 212);
+            this.lstDeck2.TabIndex = 2;
+            // 
+            // btnSortDeck1
+            // 
+            this.btnSortDeck1.Location = new System.Drawing.Point(15, 243);
+            this.btnSortDeck1.Name = "btnSortDeck1";
+            this.btnSortDeck1.Size = new System.Drawing.Size(144, 23);
+            this.btnSortDeck1.TabIndex = 3;
+            this.btnSortDeck1.Text = "Organizar Baralho 1";
+            this.btnSortDeck1.UseVisualStyleBackColor = true;
+            this.btnSortDeck1.Click += new System.EventHandler(this.BtnSortDeck1_Click);
+            // 
+            // btnShuffleDeck2
+            // 
+            this.btnShuffleDeck2.Location = new System.Drawing.Point(219, 243);
+            this.btnShuffleDeck2.Name = "btnShuffleDeck2";
+            this.btnShuffleDeck2.Size = new System.Drawing.Size(144, 23);
+            this.btnShuffleDeck2.TabIndex = 3;
+            this.btnShuffleDeck2.Text = "Embaralhar 2";
+            this.btnShuffleDeck2.UseVisualStyleBackColor = true;
+            this.btnShuffleDeck2.Click += new System.EventHandler(this.btnShuffleDeck2_Click);
+            // 
+            // btnShuffleDeck1
+            // 
+            this.btnShuffleDeck1.Location = new System.Drawing.Point(15, 272);
+            this.btnShuffleDeck1.Name = "btnShuffleDeck1";
+            this.btnShuffleDeck1.Size = new System.Drawing.Size(144, 23);
+            this.btnShuffleDeck1.TabIndex = 3;
+            this.btnShuffleDeck1.Text = "Embaralhar 1";
+            this.btnShuffleDeck1.UseVisualStyleBackColor = true;
+            this.btnShuffleDeck1.Click += new System.EventHandler(this.BtnShuffleDeck1_Click);
+            // 
+            // btnMoveToDeck2
+            // 
+            this.btnMoveToDeck2.Location = new System.Drawing.Point(165, 58);
+            this.btnMoveToDeck2.Name = "btnMoveToDeck2";
+            this.btnMoveToDeck2.Size = new System.Drawing.Size(48, 24);
+            this.btnMoveToDeck2.TabIndex = 3;
+            this.btnMoveToDeck2.Text = ">>";
+            this.btnMoveToDeck2.UseVisualStyleBackColor = true;
+            // 
+            // btnMoveToDeck1
+            // 
+            this.btnMoveToDeck1.Location = new System.Drawing.Point(165, 88);
+            this.btnMoveToDeck1.Name = "btnMoveToDeck1";
+            this.btnMoveToDeck1.Size = new System.Drawing.Size(48, 24);
+            this.btnMoveToDeck1.TabIndex = 3;
+            this.btnMoveToDeck1.Text = "<<";
+            this.btnMoveToDeck1.UseVisualStyleBackColor = true;
+            // 
+            // btnSortDeck2
+            // 
+            this.btnSortDeck2.Location = new System.Drawing.Point(219, 272);
+            this.btnSortDeck2.Name = "btnSortDeck2";
+            this.btnSortDeck2.Size = new System.Drawing.Size(144, 23);
+            this.btnSortDeck2.TabIndex = 3;
+            this.btnSortDeck2.Text = "Organizar Baralho 2";
+            this.btnSortDeck2.UseVisualStyleBackColor = true;
+            this.btnSortDeck2.Click += new System.EventHandler(this.BtnSortDeck2_Click);
+            // 
+            // FormDeck
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 401);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(373, 305);
+            this.Controls.Add(this.btnMoveToDeck1);
+            this.Controls.Add(this.btnMoveToDeck2);
+            this.Controls.Add(this.btnShuffleDeck1);
+            this.Controls.Add(this.btnSortDeck2);
+            this.Controls.Add(this.btnShuffleDeck2);
+            this.Controls.Add(this.btnSortDeck1);
+            this.Controls.Add(this.lstDeck2);
+            this.Controls.Add(this.lstDeck1);
+            this.Controls.Add(this.lblDeck2);
+            this.Controls.Add(this.lblDeck1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormDeck";
+            this.Text = "Dois Baralhos";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDeck1;
+        private System.Windows.Forms.Label lblDeck2;
+        private System.Windows.Forms.ListBox lstDeck1;
+        private System.Windows.Forms.ListBox lstDeck2;
+        private System.Windows.Forms.Button btnSortDeck1;
+        private System.Windows.Forms.Button btnShuffleDeck2;
+        private System.Windows.Forms.Button btnShuffleDeck1;
+        private System.Windows.Forms.Button btnMoveToDeck2;
+        private System.Windows.Forms.Button btnMoveToDeck1;
+        private System.Windows.Forms.Button btnSortDeck2;
     }
 }
 
--- DeckOfCards/FormDeck.cs
+++ DeckOfCards/FormDeck.cs
@@ -4,9 +4,67 @@ namespace DeckOfCards
 {
     public partial class FormDeck : Form
     {
+        private Deck _deck1 = new Deck();
+        private Deck _deck2 = new Deck();
+
         public FormDeck()
         {
             InitializeComponent();
+
+            // Ao iniciar, o baralho 1 deve ter 10 cartas aleatórieas e o baralho 2
+            // deve ser um baralho completo com 52 cartas, ambos ordenados por
+            // naipe e valor
+        }
+
+        private void ResetDeck(int deckNumber)
+        {
+            // Esvazia e reinicializa o deck (1 com 10 ou 2 com 52 cartas)
+        }
+
+        private void RedrawDeck(int deckNumber)
+        {
+            if (deckNumber == 1)
+            {
+                lstDeck1.Items.Clear();
+                foreach(string cardName in _deck1.GetCardNames())
+                {
+                    lstDeck1.Items.Add(cardName);
+                }
+                lblDeck1.Text = $"Baralho 1 ({_deck1.Count} cartas)";
+            }
+            else
+            {
+                lstDeck2.Items.Clear();
+                foreach (string cardName in _deck2.GetCardNames())
+                {
+                    lstDeck2.Items.Add(cardName);
+                }
+                lblDeck2.Text = $"Baralho 1 ({_deck2.Count} cartas)";
+            }
+        }
+
+        private void BtnSortDeck1_Click(object sender, System.EventArgs e)
+        {
+            RedrawDeck(1);
+            ResetDeck(1);
+        }
+
+        private void BtnSortDeck2_Click(object sender, System.EventArgs e)
+        {
+            RedrawDeck(2);
+            ResetDeck(2);
+        }
+
+        private void BtnShuffleDeck1_Click(object sender, System.EventArgs e)
+        {
+            _deck1.Shuffle();
+            RedrawDeck(1);
+        }
+
+        private void btnShuffleDeck2_Click(object sender, System.EventArgs e)
+        {
+            _deck2.Shuffle();
+            RedrawDeck(2);
         }
     }
 }
