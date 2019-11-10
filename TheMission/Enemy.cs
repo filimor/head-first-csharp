ed81a3dd8cb929df8011c5e63590bed637ff000f
Filipe Moreira <filimor@posteo.com.br>
2019-11-10 13:13:34 -0300

    Implementada a classe Enemy

--- TheMission/Enemy.cs
+++ TheMission/Enemy.cs
@@ -3,19 +3,23 @@ using System.Drawing;
 
 namespace TheMission
 {
+    /// <summary>
+    /// // Enemies não tem um método Attack() porque seu ataque foi incluído em Move().
+    /// </summary>
     public abstract class Enemy : Mover
     {
-        // Enemies não tem um método Attack() porque seu ataque
-        // foi incluído em Move().
-
         private const int NEARPLAYERDISTANCE = 25;
 
         public int HitPoints { get; private set; }
 
-        // O formulário pode usar essa propriedade para ver se o inimigo
-        // deve ser visível na masmorra do jogo.
+        /// <summary>
+        /// O formulário usa-a para ver se o inimigo deve ser visível na masmorra do jogo.
+        /// </summary>
         public bool Dead => HitPoints <= 0;
 
+        /// <summary>
+        /// Localização atual do inimigo.
+        /// </summary>
         new public Point Location { get; private set; }
 
         protected Enemy(Game game, Point location, Rectangle boundaries, int hitPoints) : base(game, location)
@@ -23,31 +27,34 @@ namespace TheMission
             HitPoints = hitPoints;
         }
 
+        /// <summary>
+        /// Quando o jogador ataca um inimigo, ele chama o método Hit() de Enemy, que subtrai um número aleatório dos pontos de vida.
+        /// </summary>
+        /// <param name="maxDamage">Dano máximo.</param>
+        /// <param name="random">Variável tipo aleatória.</param>
         public void Hit(int maxDamage, Random random)
         {
-            // Quando o jogador ataca um inimigo, ele chama o método Hit() de
-            // Enemy, que subtrai um número aleatório dos pontos de vida.
-
             HitPoints -= random.Next(1, maxDamage);
         }
 
         public abstract void Move(Random random);
 
+        /// <summary>
+        /// Usa o método estático Mover.Nearby() para descobrir se o inimigo está perto do jogador.
+        /// </summary>
+        /// <returns>Retorna true se o inimigo está perto do jogador.</returns>
         protected bool NearPlayer()
         {
-            // O método NearPlayer() usa o método estático Mover.Nearby()
-            // para descobrir se o inimigo está perto do jogador.
-
             return Nearby(_game.PlayerLocation, NEARPLAYERDISTANCE);
         }
 
+        /// <summary>
+        /// Descobre onde o jogador está em relação ao inimigo.
+        /// </summary>
+        /// <param name="playerLocation"></param>
+        /// <returns>Retorna em qual direção o inimigo precisa se mover.</returns>
         protected Direction FindPlayerDirection(Point playerLocation)
         {
-            // A partir da localização do jogador, usa o campo location
-            // da classe base para descobrir onde o jogador está em relação
-            // ao inimigo e retornará uma enum Direction que informa em qual
-            // direção o inimigo precisa se mover para se aproximar do jogador.
-
             return playerLocation.X > Location.X + 10
                 ? Direction.Right : playerLocation.X < Location.X - 10
                 ? Direction.Left : playerLocation.Y < Location.Y - 10
