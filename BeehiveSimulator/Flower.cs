6386a71783aa736db3d5710ca5830b971c006aa6
Filipe Moreira <filimor@posteo.com.br>
2019-11-16 18:19:26 -0300

    Implementada a classe Flower

--- BeehiveSimulator/Flower.cs
+++ BeehiveSimulator/Flower.cs
@@ -9,12 +9,12 @@ namespace BeehiveSimulator
 {
     public class Flower
     {
-        private const int LIFESPANMIN = 15000;
-        private const int LIFESPANMAX = 30000;
-        private const double INITIALNECTAR = 1.5;
-        private const double MAXNECTAR = 5;
-        private const double NECTARADDEDPERTURN = 0.01;
-        private const double NECTARGATHEREDPERTURN = 0.03;
+        private const int LIFESPANMIN = 15000; // Mínimo de vida da flor
+        private const int LIFESPANMAX = 30000; // Máximo de vida da flor
+        private const double INITIALNECTAR = 1.5; // Néctar inicial da flor
+        private const double MAXNECTAR = 5.0; // Máximo de néctar que a flor pode conter
+        private const double NECTARADDEDPERTURN = 0.01; // Néctar adicionado ao envelhecer
+        private const double NECTARGATHEREDPERTURN = 0.3; // Néctar recolhido por turno
 
         private int _lifespan;
 
@@ -37,6 +37,17 @@ namespace BeehiveSimulator
             // coletada num ciclo do total restante da flor e retornar quanto foi
             // coletado. Adicionar a quantidade colhida a NectarHarvested, que manterá
             // o total de néctar coletado de cada flor em particular.
+
+            if ( NECTARGATHEREDPERTURN > Nectar)
+            {
+                return 0;
+            }
+            else
+            {
+                Nectar -= NECTARGATHEREDPERTURN;
+                NectarHarvested += NECTARADDEDPERTURN;
+                return NECTARGATHEREDPERTURN;
+            }
         }
 
         public void Go()
@@ -47,6 +58,19 @@ namespace BeehiveSimulator
             // Se a flor continuar viva, será preciso adicionar a quantidade de néctar
             // que cada flor produz num ciclo. Não ultrapassar o limite de néctar que
             // uma flor pode manter.
+
+            if (++Age > _lifespan)
+            {
+                Alive = false;
+            }
+            else
+            {
+                Nectar += NECTARADDEDPERTURN;
+                if (Nectar > MAXNECTAR)
+                {
+                    Nectar = MAXNECTAR;
+                }
+            }
         }
     }
 }
