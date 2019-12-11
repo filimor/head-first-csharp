a3837a710874e8146d34b5485440907cf8814529
Filipe Moreira <filimor@posteo.com.br>
2019-12-11 13:17:14 -0300

    Iniciando a implementacao da classe Shot

--- SpaceInvaders/Shot.cs
+++ SpaceInvaders/Shot.cs
@@ -9,14 +9,40 @@ namespace SpaceInvaders
 {
     public class Shot
     {
-        public void Draw(Graphics g)
-        {
+        // Esses valores podem ser alterados para tornar o jogo mais fácil ou mais difícil.
+        private const int MOVEINTERVAL = 20;
+        private const int WIDTH = 5;
+        private const int HEGHT = 15;
+        private Direction _direction;
+        private Rectangle _boundaries;
 
+        public Point Location { get; private set; }
+
+        public Shot(Point location, Direction direction, Rectangle boundaries)
+        {
+            Location = location;
+            _direction = direction;
+            _boundaries = boundaries;
         }
 
+        /// <summary>
+        /// Desenha um pequeno retângulo amarelo que é o disparo.
+        /// </summary>
+        /// <param name="g">Objeto Graphics</param>
+        public void Draw(Graphics g)
+        {
+            
+        }
+
+        /// <summary>
+        /// Movimenta o tiro para cima ou para baixo e verifica se o tiro está dentro
+        /// dos limites da tela.
+        /// </summary>
+        /// <param name="direction">Direção do tiro.</param>
+        /// <returns>Retorna true se o tiro ainda estiver dentro dos limites do jogo.</returns>
         public bool Move(Direction direction)
         {
-
+            
         }
     }
 }
