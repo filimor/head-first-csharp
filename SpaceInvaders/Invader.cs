058a78a1f08cea94da0fac7d842244911a24d23e
Filipe Moreira <filimor@posteo.com.br>
2019-12-11 12:42:01 -0300

    Comecando a implementar a classe Invader

--- SpaceInvaders/Invader.cs
+++ SpaceInvaders/Invader.cs
@@ -9,14 +9,63 @@ namespace SpaceInvaders
 {
     public class Invader
     {
-        public Rectangle Area { get; set; }
+        /// <summary>
+        /// Quantos pixels um invasor de move a cada vez que ele dá um passo.
+        /// </summary>
+        private const int HORIZONTALINTERVAL = 10;
+        /// <summary>
+        /// O número de pixels que um invasor desce quando a formação chega à borda do
+        /// campo de batalha.
+        /// </summary>
+        private const int VERTICALINTERVAL = 40;
+        private Bitmap image;
 
+        public Point Location { get; private set; }
+        public Type InvaderType { get; private set; }
+        public Rectangle Area => new Rectangle(Location, image.Size);
+        public int Score { get; private set; }
+        public enum Type
+        {
+            Bug,
+            Saucer,
+            Satellite,
+            Spaceship,
+            Star
+        }
+
+        public Invader(Type invaderType, Point location, int score)
+        {
+            InvaderType = invaderType;
+            Location = location;
+            Score = score;
+            image = InvaderImage(0);
+        }
+
+        /// <summary>
+        /// Move a nave na direção especificada.
+        /// </summary>
+        /// <param name="direction">Direção do movimento.</param>
         public void Move(Direction direction)
         {
 
         }
 
+        /// <summary>
+        /// Faz o invasor desenhar a si mesmo na área de jogo.
+        /// </summary>
+        /// <param name="g">Objeto Graphics.</param>
+        /// <param name="animationCell">Célula de animação.</param>
         public void Draw(Graphics g, int animationCell)
+        {
+            InvaderImage(animationCell);
+        }
+
+        /// <summary>
+        /// Retorna o bitmap correto para a célula especificada.
+        /// </summary>
+        /// <param name="animationCell">Célula de animação.</param>
+        /// <returns></returns>
+        private Bitmap InvaderImage(int animationCell)
         {
 
         }
