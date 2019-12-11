492315e8532c116f64f3163bdfa33872f833940a
Filipe Moreira <filimor@posteo.com.br>
2019-12-11 13:29:22 -0300

    Iniciando a implementacao da classe Stars

--- SpaceInvaders/Stars.cs
+++ SpaceInvaders/Stars.cs
@@ -9,15 +9,54 @@ namespace SpaceInvaders
 {
     public class Stars
     {
-        public List<Star> StarsOnScreen { get; set; }
-        public void Twinkle()
+        private struct Star
         {
-            // Remove cinco estrelas em posições aleatórias e adiciona cinco novas.
+            public Point Point;
+            public Pen Pen;
+
+            public Star(Point point, Pen pen)
+            {
+                Point = point;
+                Pen = pen;
+            }
+        }
+
+        /// <summary>
+        /// Armazena 300 structs Star.
+        /// </summary>
+        private List<Star> StarsOnScreen { get; private set; }
+
+        /// <summary>
+        /// Povoa a lista de estrelas.
+        /// </summary>
+        /// <param name="boundaries">Rectangle com os limites de exibição.</param>
+        /// <param name="random">Instância de Random para criá-las em posições aleatórias.</param>
+        public Stars(Rectangle boundaries, Random random)
+        {
+
         }
 
         public void Draw(Graphics g)
         {
 
+        }
+
+        /// <summary>
+        /// Remove cinco estrelas em posições aleatórias e adiciona cinco novas.
+        /// </summary>
+        /// <param name="random">Instância de Random.</param>
+        public void Twinkle(Random random)
+        {
+
+        }
+
+        /// <summary>
+        /// Dá uma cor aleatória às estrelas quando criadas.
+        /// </summary>
+        /// <returns>Retorna uma das cinco cores possíveis.</returns>
+        private Pen RandomPen()
+        {
+            // Gera um número de 0 a 4 e seleciona um objeto Pen correspondente.
         }
     }
 }
